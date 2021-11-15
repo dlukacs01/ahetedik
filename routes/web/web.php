8db5b163e09613e4d0de8acb43eddbafca52502f
DavidLukacs <lukacs.dvid@gmail.com>
2021-11-15 14:56:42 +0100

    design round01 specific post page completed

--- routes/web/web.php
+++ routes/web/web.php
@@ -8,7 +8,7 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/szerzoknek', 'HomeController@szerzoknek')->name('home.szerzoknek');
 Route::get('/categories', 'CategoryController@index_front')->name('category.front.index');
 Route::get('/categories/{category_slug}', 'WorkController@work_category')->name('work.category');
-Route::get('/post/{post_slug}', 'PostController@show')->name('post'); // route model binding (post instead of post id)
+Route::get('/cikkek/{post_slug}', 'PostController@show')->name('post'); // route model binding (post instead of post id)
 Route::get('/work/{work_slug}', 'WorkController@show')->name('work');
 Route::get('/szerzok', 'UserController@index_front')->name('user.front.index'); // view all users in front
 Route::get('/szerzok/{username}', 'UserController@show')->name('user.profile.show'); // user profile page in front
