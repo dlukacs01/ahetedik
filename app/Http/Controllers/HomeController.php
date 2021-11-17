8edcc0fd3fa26999fa2cc7bef0aa062ffd829737
DavidLukacs <lukacs.dvid@gmail.com>
2021-11-17 14:26:29 +0100

    design round01 search in front bugfix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,6 +37,7 @@ class HomeController extends Controller
         return view('home', ['posts'=>$posts]);
     }
     public function search(){
+        Carbon::setLocale('hu');
 
         // Get the search value from the request
         $search = request('search');
