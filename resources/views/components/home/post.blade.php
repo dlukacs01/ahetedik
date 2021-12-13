3b77230b45cf828392cdc513c3f686f4ba532df6
DavidLukacs <lukacs.dvid@gmail.com>
2021-12-13 09:32:50 +0100

    round05_two_bugs_fixed

--- resources/views/components/home/post.blade.php
+++ resources/views/components/home/post.blade.php
@@ -5,8 +5,8 @@
         <div class="card my-2 mx-2">
             @if($heading->type == 'egyeb')
 
+                <h6 class="card-header">Rovat: {{$heading->title}}</h6>
                 @foreach($heading->articles as $article)
-                    <h6 class="card-header">Rovat: {{$heading->title}}</h6>
                     <div class="card my-2 mx-2">
                         <h6 class="card-header">Cikk: {{$article->title}}</h6>
                         <div class="card-body">{!! $article->body !!}</div>
@@ -17,7 +17,7 @@
             @if($heading->type == 'muvek')
                 <h6 class="card-header">Rovat: {{$heading->title}}</h6>
                 <div class="card my-2 mx-2 border-0">
-                    <div class="d-flex justify-content-between flex-wrap p-1 text-white">
+                    <div class="d-flex justify-content-between flex-wrap p-1">
                         @foreach($heading->articles as $article)
                             <div class="p-2 article-item border mt-1">
                                 <x-home.article-card :article="$article"></x-home.article-card>
