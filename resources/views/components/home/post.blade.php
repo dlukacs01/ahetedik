58d19df376225459282a0dac4296d86f8d2cbd8b
DavidLukacs <lukacs.dvid@gmail.com>
2022-03-11 11:49:58 +0100

    v0.6 - lapszam, rovat, cikk words removed from card titles

--- resources/views/components/home/post.blade.php
+++ resources/views/components/home/post.blade.php
@@ -1,21 +1,21 @@
 <div class="card my-4">
-    <h5 class="card-header text-center">Lapszám: {{$post->title}}</h5>
+    <h5 class="card-header text-center">{{$post->title}}</h5>
 
     @foreach($post->headings as $heading)
         <div class="card my-2 mx-2">
             @if($heading->type == 'egyeb')
 
-                <h6 class="card-header">Rovat: {{$heading->title}}</h6>
+                <h6 class="card-header">{{$heading->title}}</h6>
                 @foreach($heading->articles as $article)
                     <div class="card my-2 mx-2">
-                        <h6 class="card-header">Cikk: {{$article->title}}</h6>
+                        <h6 class="card-header">{{$article->title}}</h6>
                         <div class="card-body">{!! $article->body !!}</div>
                     </div>
                 @endforeach
 
             @endif
             @if($heading->type == 'muvek')
-                <h6 class="card-header">Rovat: {{$heading->title}}</h6>
+                <h6 class="card-header">{{$heading->title}}</h6>
                 <div class="card my-2 mx-2 border-0">
                     <div class="d-flex justify-content-between flex-wrap p-1">
                         @foreach($heading->articles as $article)
