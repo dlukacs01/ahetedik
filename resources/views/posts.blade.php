f6607c5ebe77e7c0ba03a66a95bf17449125dcf6
David Lukacs <lukacs.dvid@gmail.com>
2023-06-10 19:50:11 +0200

    v0.7.1 - posts page link to the old .HU site removed

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,11 +6,6 @@
 
                 <h1 class="mt-4 mb-3">Korábbi lapszámok</h1>
 
-                <div class="alert alert-info my-4">
-                    <strong>Figyelem! </strong>
-                    A régebbi lapszámok <a href="{{ config('custom.older_posts_url') }}" target="_blank">ezen az oldalon</a> érhetőek el.
-                </div>
-
                 <x-home.posts :posts="$posts"></x-home.posts>
 
                 <x-home.pagination :objects="$posts"></x-home.pagination>
