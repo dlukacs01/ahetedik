c094dc967e721311ae6b4f8867e6a87b45271701
dlukacs01 <lukacs.dvid@gmail.com>
2024-09-01 19:45:04 +0200

    v0.7.2 - laravel file manager added to tinymce 7 self hosted

---
+++ public/js/custom/admin/tinymce-config-lfm.js
@@ -0,0 +1,33 @@
+var editor_config = {
+    path_absolute : "/",
+    selector: "textarea",
+    plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons accordion',
+    toolbar: "undo redo | accordion accordionremove | blocks fontfamily fontsize | bold italic underline strikethrough | align numlist bullist | link image | table media | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | pagebreak anchor codesample | ltr rtl",
+    relative_urls: false,
+    file_picker_callback : function(callback, value, meta) {
+        var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+        var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+        var cmsURL = editor_config.path_absolute + 'laravel-filemanager?editor=' + meta.fieldname;
+        if (meta.filetype == 'image') {
+            cmsURL = cmsURL + "&type=Images";
+        } else {
+            cmsURL = cmsURL + "&type=Files";
+        }
+
+        tinyMCE.activeEditor.windowManager.openUrl({
+            url : cmsURL,
+            title : 'Filemanager',
+            width : x * 0.8,
+            height : y * 0.8,
+            resizable : "yes",
+            close_previous : "no",
+            onMessage: (api, message) => {
+                callback(message.content);
+            }
+        });
+    },
+    language: 'hu_HU',
+};
+
+tinymce.init(editor_config);
--- resources/views/components/admin/tinymce.blade.php
+++ resources/views/components/admin/tinymce.blade.php
@@ -1,4 +1,5 @@
 {{-- SELF-HOSTED --}}
 <script src="{{ asset('js/admin/tinymce/js/tinymce/tinymce.min.js') }}"></script>
 {{-- <script src="{{ asset('js/custom/tiny-mce/langs/hu_HU.js') }}"></script> --}}
-<script src="{{ asset('js/custom/admin/tinymce-config.js') }}"></script>
+{{-- <script src="{{ asset('js/custom/admin/tinymce-config.js') }}"></script> --}}
+<script src="{{ asset('js/custom/admin/tinymce-config-lfm.js') }}"></script>
